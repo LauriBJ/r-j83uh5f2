558146faa3d18f82b6610321cdc075c654466c89
LauriBJ <laurita.buttner@gmail.com>
2023-02-03 23:54:42 -0300

    Al fin cuenta

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contador: 0,
+      textCount: (e) => {
+        this.setState({ contador: e.target.value.length });
+      },
+    };
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.state.textCount}></textarea>
+        <div className="counter">{this.state.contador}</div>
       </div>
     );
   }
